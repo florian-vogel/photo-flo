c463bf8d00cfa71f3de8cac5bd47446490a229d1
Florian Vogel <fl_vogel@web.de>
2024-06-27 19:25:59 +0200

    finished animation, iter and other fonts

--- src/js/main.js
+++ src/js/main.js
@@ -20,17 +20,45 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.querySelectorAll(".toggle-submenu").forEach((item) => {
-  item.addEventListener("click", function () {
-    const submenu = this.nextElementSibling;
+  item.addEventListener("click", function() {
+    let parentElement = this.parentElement;
+    let submenu = parentElement.querySelector(".submenu");
+    let submenuItems = submenu.querySelectorAll("li");
+    let animationDelaySubmenuItems = 200;
+
+    if (submenu.classList.contains("opening") || submenu.classList.contains("closing")) {
+      return;
+    }
+
     if (submenu.classList.contains("active-submenu")) {
+      submenu.classList.remove("active-submenu");
       submenu.classList.add("closing");
       submenu.style.maxHeight = "0";
+
+      submenuItems.forEach((item, index) => {
+        setTimeout(() => {
+          item.style.opacity = 0; // Trigger CSS transition
+        }, animationDelaySubmenuItems * (submenuItems.length - index));
+      });
+
       setTimeout(() => {
-        submenu.classList.remove("active-submenu", "closing");
-      }, 1000); // Match the longest animation delay and duration
+        submenu.classList.remove("closing");
+      }, (submenuItems.length + 1) * animationDelaySubmenuItems);
     } else {
-      submenu.classList.add("active-submenu");
-      submenu.style.maxHeight = "75px"; // Or the max height needed
+      submenu.classList.add("opening");
+      let submenuHeight = submenuItems.length * 25; // Example height per item
+      submenu.style.maxHeight = `${submenuHeight}px`;
+
+      submenuItems.forEach((item, index) => {
+        setTimeout(() => {
+          item.style.opacity = 1; // Trigger CSS transition
+        }, animationDelaySubmenuItems * (index + 1));
+      });
+
+      setTimeout(() => {
+        submenu.classList.remove("opening");
+        submenu.classList.add("active-submenu");
+      }, submenuItems.length * animationDelaySubmenuItems);
     }
   });
 });
