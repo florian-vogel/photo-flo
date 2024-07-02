e91425b00aa15a4c63862c1e89255ae273592c57
Florian Vogel <fl_vogel@web.de>
2024-07-02 16:20:29 +0200

    wip

--- src/pages/photos.php
+++ src/pages/photos.php
@@ -1,10 +1,10 @@
 <div class='photos-container flex-container'>
-<!--     <h3>Spring in Sicily</h3> -->
+    <!--     <h3>Spring in Sicily</h3> -->
     <div class="photo-container flex-container">
-        <img src="./assets/berlin_at_night/CIMG0825.JPG" alt="Description of the image">
         <div class="subtitle">San Vito Lo Capo</div>
+        <img src="./assets/berlin_at_night/CIMG0825.JPG" alt="Description of the image">
     </div>
-<!--     <div class="photo-container flex-container">
+    <!--     <div class="photo-container flex-container">
         <img src="./assets/berlin_at_night/CIMG0721.JPG" alt="Description of the image">
         <div class="subtitle">Palermo</div>
     </div>
