5e2a6d31370535bf7e737d72d354f2c8fde58f14
Florian Vogel <fl_vogel@web.de>
2024-07-08 14:18:12 +0200

    change submenu open/close mechanism wip

--- src/js/main.js
+++ src/js/main.js
@@ -24,10 +24,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.querySelectorAll(".toggle-submenu").forEach((item, index) => {
   item.addEventListener("mouseover", function () {
+    console.log("index: ", index, ", true");
     menuOpen[index] = true;
+    applyMenuState();
+  });
+});
 
-    let parentElement = this.parentElement;
-    let submenu = parentElement.querySelector(".submenu");
+document.querySelectorAll(".menu-item").forEach((item, index) => {
+  item.addEventListener("mouseleave", function () {
+    console.log("index: ", index, ", false");
+    menuOpen[index] = false;
+    applyMenuState();
+  });
+});
+
+function applyMenuState() {
+  applyOpenedItems();
+  applyClosedItems();
+}
+
+var menuItems = document.querySelectorAll(".menu-item");
+
+function applyOpenedItems() {
+  menuItems.forEach((item, index) => {
+    if (!menuOpen[index]) {
+      return;
+    }
+
+    //let parentElement = item.parentElement;
+    let submenu = item.querySelector(".submenu");
     let submenuItems = submenu.querySelectorAll("li");
     let animationDelaySubmenuItems = 200;
 
@@ -54,41 +79,30 @@ document.querySelectorAll(".toggle-submenu").forEach((item, index) => {
         submenu.classList.remove("opening");
         submenu.classList.add("active-submenu");
         applyMenuState();
-      }, submenuItems.length * animationDelaySubmenuItems);
+      }, (submenuItems.length + 1) * animationDelaySubmenuItems);
     }
   });
-});
-
-document.querySelectorAll(".menu-item").forEach((item, index) => {
-  item.addEventListener("mouseleave", function () {
-    menuOpen[index] = false;
-    applyMenuState();
-  });
-});
-
-var menuItems = document.querySelectorAll(".menu-item");
-
-function applyMenuState() {
-  //applyOpenedItems();
-  applyClosedItems();
 }
 
+var menuToggles = document.querySelectorAll(".toggle-submenu");
+
 function applyClosedItems() {
-  menuItems.forEach((item, index) => {
-    if(menuOpen[index]){
+  menuToggles.forEach((item, index) => {
+    if (menuOpen[index]) {
       return;
     }
     let parentElement = item.parentElement;
-    let submenu = item.querySelector(".submenu");
+    let submenu = parentElement.querySelector(".submenu");
     let submenuItems = submenu.querySelectorAll("li");
     let animationDelaySubmenuItems = 200;
 
-    /*     if (
+    if (
       submenu.classList.contains("opening") ||
       submenu.classList.contains("closing")
     ) {
       return;
-    } */
+    }
+
     if (submenu.classList.contains("active-submenu")) {
       submenu.classList.remove("active-submenu");
       submenu.classList.add("closing");
@@ -105,6 +119,7 @@ function applyClosedItems() {
 
       setTimeout(() => {
         submenu.classList.remove("closing");
+        applyMenuState();
       }, (submenuItems.length + 1) * animationDelaySubmenuItems);
     }
   });
