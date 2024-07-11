a1ca358b3411b5bcb2df927a47a03e61e2338928
Florian Vogel <fl_vogel@web.de>
2024-07-12 01:18:17 +0200

    first design finished

--- src/pages/photos.php
+++ src/pages/photos.php
@@ -1,7 +1,7 @@
 <div class='photos-container flex-container'>
     <div class="photo-container flex-container">
-        <div id="carouselDescription" class="subtitle">San Vito Lo Capo</div>
         <img id="carouselImage" src="" alt="">
+        <div id="carouselDescription" class="subtitle">San Vito Lo Capo</div>
     </div>
     <!--     <button id="prevButton">Previous</button>
     <button id="nextButton">Next</button> -->
