d8f5a0130d5daf6f02a7a63572b52b596c9021d9
Florian Vogel <fl_vogel@web.de>
2024-07-11 15:29:40 +0200

    navigation menue automatic generation

--- src/js/navigationMenu.js
+++ src/js/navigationMenu.js
@@ -3,7 +3,7 @@ const config = {
   categories: [
     {
       name: "liva voigt",
-      images: ["./assets/berlin_at_night/bspbildhorizontal.jpg"],
+      images: ["./assets/photo/barcelona/bspbildhorizontal.jpg", "./assets/exhibitions/blendung/img_4697.jpg"],
       subcategories: [],
     },
     {
@@ -55,3 +55,109 @@ function generateMenu(categories) {
 }
 
 generateMenu(config.categories);
+
+
+
+
+document.querySelectorAll(".toggle-submenu").forEach((item, index) => {
+  item.addEventListener("mouseover", function () {
+    console.log("index: ", index, ", true");
+    menuOpen[index] = true;
+    applyMenuState();
+  });
+});
+
+document.querySelectorAll(".menu-item").forEach((item, index) => {
+  item.addEventListener("mouseleave", function () {
+    console.log("index: ", index, ", false");
+    menuOpen[index] = false;
+    applyMenuState();
+  });
+});
+
+function applyMenuState() {
+  applyOpenedItems();
+  applyClosedItems();
+}
+
+var menuItems = document.querySelectorAll(".menu-item");
+
+function applyOpenedItems() {
+  menuItems.forEach((item, index) => {
+    if (!menuOpen[index]) {
+      return;
+    }
+
+    //let parentElement = item.parentElement;
+    let submenu = item.querySelector(".submenu");
+    let submenuItems = submenu.querySelectorAll("li");
+    let animationDelaySubmenuItems = 200;
+
+    if (
+      submenu.classList.contains("opening") ||
+      submenu.classList.contains("closing")
+    ) {
+      return;
+    }
+
+    if (!submenu.classList.contains("active-submenu")) {
+      submenu.classList.add("opening");
+      let submenuHeight = submenuItems.length * 15; // Example height per item
+      submenu.style.maxHeight = `${submenuHeight}px`;
+
+      submenuItems.forEach((item, index) => {
+        item.style.display = "block";
+        setTimeout(() => {
+          item.style.opacity = 1; // Trigger CSS transition
+        }, animationDelaySubmenuItems * (index));
+      });
+
+      setTimeout(() => {
+        submenu.classList.remove("opening");
+        submenu.classList.add("active-submenu");
+        applyMenuState();
+      }, (submenuItems.length + 1) * animationDelaySubmenuItems);
+    }
+  });
+}
+
+var menuToggles = document.querySelectorAll(".toggle-submenu");
+
+function applyClosedItems() {
+  menuToggles.forEach((item, index) => {
+    if (menuOpen[index]) {
+      return;
+    }
+    let parentElement = item.parentElement;
+    let submenu = parentElement.querySelector(".submenu");
+    let submenuItems = submenu.querySelectorAll("li");
+    let animationDelaySubmenuItems = 200;
+
+    if (
+      submenu.classList.contains("opening") ||
+      submenu.classList.contains("closing")
+    ) {
+      return;
+    }
+
+    if (submenu.classList.contains("active-submenu")) {
+      submenu.classList.remove("active-submenu");
+      submenu.classList.add("closing");
+      submenu.style.maxHeight = "0";
+
+      submenuItems.forEach((item, index) => {
+        setTimeout(() => {
+          item.style.opacity = 0; // Trigger CSS transition
+          setTimeout(() => {
+            item.style.display = "none";
+          }, 300);
+        }, animationDelaySubmenuItems * (submenuItems.length - index - 1));
+      });
+
+      setTimeout(() => {
+        submenu.classList.remove("closing");
+        applyMenuState();
+      }, (submenuItems.length + 1) * animationDelaySubmenuItems);
+    }
+  });
+}
--- src/pages/photos.php
+++ src/pages/photos.php
@@ -1,7 +1,7 @@
 <div class='photos-container flex-container'>
     <div class="photo-container flex-container">
         <div class="subtitle">San Vito Lo Capo</div>
-        <img id="carouselImage" src="./assets/berlin_at_night/IMG_4697.JPG" alt="Description of the image">
+        <img id="carouselImage" src="" alt="Description of the image">
     </div>
     <!--     <button id="prevButton">Previous</button>
     <button id="nextButton">Next</button> -->
