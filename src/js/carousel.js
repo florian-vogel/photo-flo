0e6ef7614f74ecdd1fe53a6a68c5421c497ff0ab
Florian Vogel <fl_vogel@web.de>
2024-07-13 17:51:49 +0200

    add transition animation between categories

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -5,17 +5,20 @@ const descriptionElement = document.getElementById("carouselDescription");
 function loadCarousel(newImages) {
   images = newImages; // Set the new image array
   currentIndex = 0; // Reset index to start from the first image
-  imgElement.src = images[currentIndex].url; // Load the first image
-  imgElement.classList.add("fade-in"); // Add fade-in to ensure the first image animates in
+  //imgElement.src = images[currentIndex].url; // Load the first image
   descriptionElement.innerText = images[currentIndex].description;
+  changeImage('stay');
 }
 
-function changeImage(forward = true) {
+function changeImage(direction = "forward") {
   imgElement.classList.remove("fade-in");
   setTimeout(() => {
-    currentIndex = forward
-      ? (currentIndex + 1) % images.length
-      : (currentIndex - 1 + images.length) % images.length;
+    currentIndex =
+      direction == "forward"
+        ? (currentIndex + 1) % images.length
+        : direction == "backward"
+        ? (currentIndex - 1 + images.length) % images.length
+        : currentIndex;
     imgElement.src = images[currentIndex].url;
     imgElement.classList.add("fade-in");
     descriptionElement.innerText = images[currentIndex].description;
@@ -24,7 +27,7 @@ function changeImage(forward = true) {
 
 document
   .getElementById("carouselImage")
-  .addEventListener("click", () => changeImage(true));
+  .addEventListener("click", () => changeImage('forward'));
 document
   .getElementById("prevButton")
-  .addEventListener("click", () => changeImage(false));
+  .addEventListener("click", () => changeImage('backward'));
